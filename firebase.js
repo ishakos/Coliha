bcbc2e7fd6b448cb24996d9933e1970d56558c34
Ferdjani Abdessalam Ishak <moh.ferdjani@gmail.com>
2025-07-20 18:06:29 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,9 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
-const firebaseConfig = {
-
-};
+const firebaseConfig = {};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
